17af173b907e08ff488ce1cad5b9876d149c2219
Shristi Gupta <gshristi40@gmail.com>
2023-11-22 22:35:04 +0530

    fixed height and width issues

--- src/Home/Components/Todo.js
+++ src/Home/Components/Todo.js
@@ -22,23 +22,18 @@ export const Todo = () => {
     });
     setInputValue("");
   };
-
-  const [isChecked, setIsChecked] = useState(null);
-
-  const handleCheckboxChange = (index) => {
-    setIsChecked(index); // Toggle the checked state
-  };
+  
   return (
     <>
-      <div className="h-72 md:h-96">
-        <div className="fixed top-0 w-full left-0 h-72 md:h-96 ">
-          <div className=" w-full relative h-72 md:h-96 z-0  ">
+      <div className="h-72 md:h-96 z-10 relative">
+        <div className="fixed top-0 w-full left-0 h-72 md:h-96">
+          <div className=" w-full relative  z-0  ">
             <div className=" bg-black w-full z-0 h-72 md:h-96"></div>
             <div className="w-full h-full top-0 opacity-20 absolute bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></div>
             <h1 className="text-white w-full text-center font-bold text-3xl md:text-6xl absolute bottom-16  uppercase tracking-widest">
               To-Do List
             </h1>
-            <div className="left-1/2 flex gap-5 -translate-x-1/2 absolute bottom-0 translate-y-1/2 ">
+            <div className="left-1/2 flex gap-3 md:gap-5 -translate-x-1/2 absolute bottom-0 translate-y-1/2 ">
               <input
                 className=" h-12 md:w-[30rem] shadow-sm rounded-lg bg-slate-300 px-4 placeholder:px-4 focus:outline-none"
                 onKeyPress={click}
@@ -46,15 +41,15 @@ export const Todo = () => {
                 value={inputValue}
                 placeholder="Create a new to-do"
               />
-              <button type="submit" onClick={handleButtonClick} className=" bg-slate-300 flex items-center  justify-center text-3xl h-12 w-12 rounded-full">+</button>
+              <button type="submit" onClick={handleButtonClick} className=" bg-slate-300 text-2xl h-12 w-12 rounded-full shadow-sm">+</button>
             </div>
           </div>
         </div>
       </div>
-      <div className="max-w-6xl mx-auto flex flex-wrap pt-20 gap-6">
+      <div className=" relative z-0 max-w-6xl mx-auto flex justify-center md:justify-start flex-wrap pt-12 md:pt-20 gap-4 md:gap-6">
         {data.map((item,index) => (
-          <div key={index} className="col-span-1 whitespace-normal w-80 flex gap-4 h-fit bg-zinc-400   p-5 rounded-lg">
-            <input checked={isChecked === index} onChange={()=>handleCheckboxChange(index)} type="checkbox" id="" /> <p className=" capitalize">{item.msg}</p> 
+          <div key={index} className="col-span-1 whitespace-normal min-w-[20rem] flex gap-4 h-fit bg-zinc-400 p-4 md:p-5 rounded-lg shadow-md">
+            <input type="checkbox" id="" className=" scale-125" /> <p className=" capitalize">{item.msg}</p> 
           </div>
         ))}
       </div>
